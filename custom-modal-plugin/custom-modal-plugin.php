3435fb11f1487f526295f9510d90a53d5b0b6af7
Manuel Moreno <jmmoreno@rotoplas.com>
2024-08-01 10:00:03 -0600

    FIX: Se agrega cookie para mostrar modal de mayores de edad

--- custom-modal-plugin/custom-modal-plugin.php
+++ custom-modal-plugin/custom-modal-plugin.php
@@ -18,4 +18,30 @@ function custom_modal_enqueue_scripts() {
 }
 add_action('wp_enqueue_scripts', 'custom_modal_enqueue_scripts');
 
+// Shortcode para mostrar el modal
+function custom_modal_add_modal_to_footer() {
+    ?>
+    <div id="custom-modal" class="custom-modal">
+        <div class="custom-modal-content">
+            <h2>¿Eres mayor de edad?</h2>
+            <h4>En este sitio encontaras los mejores servicios para tu evetos.</h4><br>
+            <h4>¡Arma tu peda sin pedos!</h4>
+            <div class="sc_services_item_button sc_item_button">
+                <a href="#" id="btn-yes" class="sc_button color_style_default sc_button_default sc_button_size_normal custom-modal-btn">Soy mayor de edad</a>
+            </div>
+            <div class="sc_services_item_button sc_item_button">
+                <a class="sc_button color_style_default sc_button_default sc_button_size_normal custom-modal-btn"
+                   href="#" id="btn-no">Soy menor de edad</a>
+            </div>
+            <div class="custom-modal-footer">
+                <p>Al aceptar que es mayor de edad usted acepta
+                    los <a href="#">Términos y Condiciones</a> y el <a href="https://tlalocalli.com/politica-de-privacidad/">Aviso de privacidad</a> y cookies Tlalocalli. <a href="www.alcoholinformate.org.mx" target="_blank">www.alcoholinformate.org.mx</a></p>
+            </div>
+        </div>
+    </div>
+    <?php
+}
+add_action('wp_footer', 'custom_modal_add_modal_to_footer');
+
+
 ?>
